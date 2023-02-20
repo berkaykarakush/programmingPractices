d848904abfcc1e1bb5a30830c976b5bddc4c88b9
berkaykarakush <berkaykaraks@outlook.com>
2023-02-20 20:48:38 +0300

    UcgenAlani added

---
+++ C#/consoleApp/ucgenAlani/ucgenAlani/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ucgenAlani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ucgen alanini hesaplayan program
+            Console.Write("Ucgenin kenar degerini giriniz: ");
+            int kenar = int.Parse(Console.ReadLine()); //kenar yukseklik degerini int'a cast et ve kenar degiskenine ata
+            Console.Write("Ucgenin yuksekligini degerini giriniz: ");
+            int yukseklik = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(kenar *yukseklik/2);//alan=kenar*yukseklik/2
+        }
+    }
+}
