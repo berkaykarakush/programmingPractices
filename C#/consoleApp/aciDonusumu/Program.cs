de1eb91bd49def85206fe4d9d7d0f20f545d5775
berkaykarakush <berkaykaraks@outlook.com>
2023-02-21 10:39:54 +0300

    aciDonusumu added

---
+++ C#/consoleApp/aciDonusumu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace aciDonusumu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //derece cinsinden girilen aciyi radyan ve gradyana donusturme
+
+            const float pi = 3.14f; //pi sayisi sabit olarak olusturuldu
+
+            Console.Write("Aci degerini giriniz (derece): ");
+            float derece = float.Parse(Console.ReadLine()); //girilen degeri int'e cast et ve ata
+
+            Console.WriteLine("Radyan degeri: " + derece*pi/180); //radyan=derece*pi/180
+            Console.WriteLine("Gradyan degeri: " +derece*200/180); //gradyan=derece*200/180
+        }
+    }
+}
