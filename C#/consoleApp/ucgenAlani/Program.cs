7aba7ac5afa53df1edb86561134ad0854a03b273
berkaykarakush <berkaykaraks@outlook.com>
2023-02-21 10:32:16 +0300

    ucgenAlani updated

--- C#/consoleApp/ucgenAlani/Program.cs
+++ C#/consoleApp/ucgenAlani/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ucgenAlani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bir kenar uzunlugu ve yukseklik degeri girilen ucgenin alani
+
+            Console.Write("Kenar degerini giriniz: ");//bilgi metni
+            int kenar = int.Parse(Console.ReadLine()); //girilen degeri int'e cast et ve ata
+
+            Console.Write("Yukseklik degerini giriniz: "); 
+            int yukseklik = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ucgenin alani: " + kenar*yukseklik/2); //alan=kenar*yukseklik/2
+        }
+    }
+}
