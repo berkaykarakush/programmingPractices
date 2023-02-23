bbe955d2752d6c2e02082a7d11d683e2f7e27e60
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 12:41:05 +0300

    kureAlanHacim added

---
+++ C#/consoleApp/kureAlanHacim/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace kureAlanHacim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //kure alan ve hacim hesaplama 
+            Console.Write("Kurenin yaricapi degerini giriniz: "); //bilgi metni 
+            float yaricap = float.Parse(Console.ReadLine()); //girilen degeri float'a cast et ve ata
+
+            Console.WriteLine("Kurenin alani: " + (4 * Math.PI * Math.Pow(yaricap, 2)));//alan=4*pi*r^2
+            Console.WriteLine("Kurenin hacmi: " + (4 * Math.PI * Math.Pow(yaricap, 3) / 3));//hacim=(4*pi*r^3)/3
+
+        }
+    }
+}
