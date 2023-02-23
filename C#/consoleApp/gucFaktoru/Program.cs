9128137276f32d0fc5de7b94d945f4496c56adc2
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 21:07:42 +0300

    gucFaktoru added

---
+++ C#/consoleApp/gucFaktoru/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace gucFaktoru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir guc sistemine ait gerilim ve akimin Volt ile Amper cinsinden genlikleri ve derece cinsinden de faz acilari girilmektedir. Buna gore sistemdeki
+            //akitf-reaktif-gorunur gucu ve guc faktorunu hesaplayip ekrana yazdiran program
+
+            Console.Write("Volt degerini giriniz: "); //bilgi metni
+            float volt = float.Parse(Console.ReadLine()); //girilen degeri float'a cast et ve ata
+
+            Console.Write("Amper degerini giriniz: ");
+            float amper = float.Parse(Console.ReadLine());
+
+            Console.Write("Faz Acisi degerini giriniz: ");
+            float fazAcisi = float.Parse(Console.ReadLine());
+
+            fazAcisi = (float)(fazAcisi * Math.PI / 180); //fi=fi*pi/180 , ayrica bilincli tur donumusu uyguladim cunku Math class'indan double deger donmekte
+            float gorunurGuc = volt * amper; //v=s*i
+            float gucFaktoru = (float)Math.Cos(fazAcisi); //gf=cos(fi)
+            float aktifGuc = gorunurGuc * gucFaktoru; //p=s*gf
+            float reaktifGuc = gorunurGuc * (float)Math.Cos(fazAcisi); //q=s*sin(fi)
+
+            Console.WriteLine($"Aktif Guc: {aktifGuc} \nReaktif Guc: {reaktifGuc} \nGorunur Guc: {gorunurGuc} \nGuc Faktoru: {gucFaktoru}");
+        }
+    }
+}
