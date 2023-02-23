86ff3876cfc8f801b516e5d67d48905af2267d6b
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 15:24:40 +0300

    molSayisi added

---
+++ C#/consoleApp/molSayisi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace molSayisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //maddenin mol sayisini hesaplama 
+            Console.Write("Maddenin kutlesini giriniz (m): "); //bilgi metni
+            float kutle = float.Parse(Console.ReadLine()); //girilen degeri float'a cast et ve ata
+
+            Console.Write("Maddenin molekul agirligini giriniz (ma): ");
+            float molekulAgirligi = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("mol: " + kutle/molekulAgirligi); //mol=m/ma
+        }
+    }
+}
