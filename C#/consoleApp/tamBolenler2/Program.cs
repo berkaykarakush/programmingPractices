35d79108001519a9ce89ed3479377a4c558543af
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 21:45:39 +0300

    tamBolenler2 added

---
+++ C#/consoleApp/tamBolenler2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tamBolenler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pozitif bir tamsayinin tam bolenleri - hizlandirilmis versiyon
+
+            Console.Write("Bir tam sayi degeri giriniz: "); //bilgi metni
+            int sayi = int.Parse(Console.ReadLine()); //girilen degeri int'a cast et ve ata
+
+            Console.WriteLine("1");//1 yaz cunu her sayi 1'e bolunur
+            for (int i = 2; i <= sayi/2; i++) //girilen degerin yarisi kadar don
+            {
+                if (sayi % i == 0) //true
+                    Console.WriteLine(i);//yaz
+            }
+        }
+    }
+}
