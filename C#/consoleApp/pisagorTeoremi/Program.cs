e32c29b500dd7931b11be10d98adde9acb89dd0b
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 11:20:02 +0300

    pisagorTeoremi added

---
+++ C#/consoleApp/pisagorTeoremi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pisagorTeoremi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pisagor teoremi 
+            Console.Write("Birinci dik kenar degerini giriniz: "); //bilgi metni
+            int birinciKenar = int.Parse(Console.ReadLine()); //ilk degeri int'e cast et ve ata
+
+            Console.Write("Ikinci dik kenar degerini giriniz: ");
+            int ikinciKenar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hipotenus: " + (Math.Sqrt(Math.Pow(birinciKenar, 2) + Math.Pow(ikinciKenar, 2)))); //c=(a^2+b^2)^(1/2)
+        }
+    }
+}
