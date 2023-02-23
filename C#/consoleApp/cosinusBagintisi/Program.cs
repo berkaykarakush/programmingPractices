1f2e2bc7fece51c75d1a04ef5ed09a9844aa3214
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 11:28:38 +0300

    cosinusBagintisi added

---
+++ C#/consoleApp/cosinusBagintisi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cosinusBagintisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cosinus bagintisi
+            Console.Write("Birinci kenar degerini giriniz: "); //bilgi metni
+            float birinciKenar = float.Parse(Console.ReadLine()); //girilen degeri float'a cast et ve ata
+
+            Console.Write("Ikinci kenar degerini giriniz: ");
+            float ikinciKenar = float.Parse(Console.ReadLine());
+
+            Console.Write("Aradaki aci degerini giriniz: ");
+            float aciDegeri = float.Parse(Console.ReadLine());
+            //c=(a^2+b^2-2*a*b*cos(aci-pi/180)^(1/2)
+            Console.WriteLine("Ucuncu kenar degeri: " + Math.Sqrt((Math.Pow(birinciKenar, 2) + Math.Pow(ikinciKenar, 2)) - 2 * birinciKenar * ikinciKenar * Math.Cos(aciDegeri * Math.PI / 180)));
+        }
+    }
+}
