b27fee3344b88ff7c5c2bae6f31016b40c66a560
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 15:41:40 +0300

    ohmKanunu added

---
+++ C#/consoleApp/ohmKanunu/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ohmKanunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ohm kanunu
+            Console.Write("Iletkenin direncini giriniz (R): "); //bilgi metni
+            float R = float.Parse(Console.ReadLine()); //girilen degeri float'a cast et ve ata
+
+            Console.Write("Iletkenin icinden akan akim degerini giriniz (I):");
+            float I = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Gerilim: " + (R * I));//v=r*i
+        }
+    }
+}
