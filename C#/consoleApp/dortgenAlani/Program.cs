2a72df2ed6c3b587a5f788e5e15997547c4a97f3
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 11:49:31 +0300

    dortgenAlani added

---
+++ C#/consoleApp/dortgenAlani/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dortgenAlani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Birinci kosegen degerini giriniz: "); //bilgi metni
+            float birinciKosegen = float.Parse(Console.ReadLine()); //girilen degeri float'a cast et ve ata
+
+            Console.Write("Ikinci kosegen degerini giriniz: ");
+            float ikinciKosegen = float.Parse(Console.ReadLine());
+
+            Console.Write("Aradaki aci degerini giriniz: ");
+            float aciDegeri = float.Parse(Console.ReadLine());
+            //alan=e*f*sin(aci*pi/180)/2
+            Console.WriteLine("Alan: " + ((birinciKosegen * ikinciKosegen * Math.Sin(aciDegeri * Math.PI / 180)) / 2));
+        }
+    }
+}
