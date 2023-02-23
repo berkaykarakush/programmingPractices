d86b52926a5df328c2a4f8406a8801280ae22e9a
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 13:03:59 +0300

    daireDilimAlan added

---
+++ C#/consoleApp/daireDilimAlan/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace daireDilimAlan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //daire diliminin alan hesabi
+            Console.Write("Yaricap degerini giriniz: "); //bilgi metni
+            float yaricap = float.Parse(Console.ReadLine());//girilen degeri float'a cast et ve ata
+
+            Console.Write("Aci degerini giriniz (derece): ");
+            float aciDegeri = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Alan: " + ((aciDegeri * Math.PI * Math.Pow(yaricap, 2)) / 360));//alan = (aci*pi*r^2)/360
+        }
+    }
+}
