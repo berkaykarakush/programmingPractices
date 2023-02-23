83838de913398c24fe7d98bbcb4784167bc3106c
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 21:27:18 +0300

    tekCift added

---
+++ C#/consoleApp/tekCift/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tekCift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen tamsayinin tek mi cift mi oldugu hesaplama 
+            Console.Write("Bir tamsayi giriniz: "); //bilgi metni
+            int sayi = int.Parse(Console.ReadLine()); //girilen degeri int'a cast et ve ata
+
+            Console.WriteLine(sayi %2 == 0 ? "Girilen sayi cift sayidir":"Girilen sayi tek sayidir"); //sayi mod 2 esitse 0 cifft, sayi mod 2 esit degilse 0 tek
+
+        }
+    }
+}
