26000e09b67a712536bf348a6869b45f4d3e0499
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 21:35:32 +0300

    tamBolunme added

---
+++ C#/consoleApp/tamBolunme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tamBolunme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a sayisinin b sayisina tam bolunme kontrolu
+
+            Console.Write("Birinci sayi degerini giriniz: "); //bilgi metni
+            int birinciSayi = int.Parse(Console.ReadLine()); //girilen degeri int'a cast et ve ata
+
+            Console.Write("Ikinci sayi degerini giriniz: ");
+            int ikinciSayi = int.Parse(Console.ReadLine());
+            
+            //birinciSayi % ikinciSayi == 0 ise ilk durum, degilse ikinci durum tetiklenecek
+            Console.WriteLine(birinciSayi % ikinciSayi == 0 ? $"{birinciSayi} sayisi {ikinciSayi} sayisina tam bolunuyor" : $"{birinciSayi} sayisi {ikinciSayi} sayisina tam bolunmuyor");
+        }
+    }
+}
