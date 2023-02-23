c62e46d4feab76d1c4d2f42b85849a50b9c35850
berkaykarakush <berkaykaraks@outlook.com>
2023-02-23 15:02:52 +0300

    pkEnerji added:

---
+++ C#/consoleApp/pkEnerji/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace pkEnerji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //potansiyel ve kinetik enerji hesaplama 
+            Console.Write("Maddenin kutlesini giriniz(m): ");//bilgi metni
+            float m = float.Parse(Console.ReadLine()); //girilen degeri float'a cast et ve ata
+
+            Console.Write("Maddenin hizini (v) giriniz: ");
+            float v = float.Parse(Console.ReadLine());
+
+            Console.Write("Maddenin yerden yuksekligini (h) giriniz: ");
+            float h = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Potansiyel Enerji: " + (m * 9.8 * h));//ep=m*9.8*h
+            Console.WriteLine("Kinetik Enerji: " + (m * Math.Pow(v, 2) / 2)); //ek=m*v^2/2
+        }
+    }
+}
