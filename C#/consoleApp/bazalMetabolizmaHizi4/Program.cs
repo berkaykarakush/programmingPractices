def33612d557c6b54da35c7db8819fb55c29aa82
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 20:39:11 +0300

    bazalMetabolizmaHizi4 added

---
+++ C#/consoleApp/bazalMetabolizmaHizi4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace bazalMetabolizmaHizi4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mifflin-St Jeor denklemi ile bazal metabolizma hizi 
+
+            Console.Write("Kilonuzu giriniz (kg): "); //bilgi metni
+            float kilo = float.Parse(Console.ReadLine()); //girilen verileri float'a cast et ve ata
+
+            Console.Write("Boyunuzu giriniz (cm): ");
+            float boy = float.Parse(Console.ReadLine());
+
+            Console.Write("Yasinizi giriniz: ");
+            int yas = int.Parse(Console.ReadLine());
+
+            Console.Write("Cinsiyetiniz kadin mi ? (true/false): "); //true=kadin | false=erkek
+            bool cinsiyet = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine(cinsiyet == true ? $"Bazal Metabolizma Hizini: {(9.99f * kilo + 6.25f * boy - 4.92f * yas -161)}": //kadin 
+                                                 $"Bazal Metabolizma Hizini: {(9.99f * kilo + 6.25f * boy - 4.92f * yas + 5)}"); //erkek
+        }
+    }
+}
