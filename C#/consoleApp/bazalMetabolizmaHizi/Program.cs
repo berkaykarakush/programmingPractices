689cc1b51bf1afc13b7534c852b25451e59208d9
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 18:16:53 +0300

    bazalMetabolizmaHizi added

---
+++ C#/consoleApp/bazalMetabolizmaHizi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace bazalMetabolizmaHizi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Harris-Benecdict denklemi ile bazal metabolizma hizi 
+
+            Console.Write("Kilonuzu giriniz (kg): "); //bilgi metni
+            float kilo = float.Parse(Console.ReadLine()); //girilen verileri float'a cast et ve ata
+
+            Console.Write("Boyunuzu giriniz (cm): ");
+            float boy= float.Parse(Console.ReadLine());
+
+            Console.Write("Yasinizi giriniz: ");
+            int yas = int.Parse(Console.ReadLine());
+
+            Console.Write("Cinsiyetiniz kadin mi ? (true/false): "); //true=kadin | false=erkek
+            bool cinsiyet = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine(cinsiyet == true ? $"Bazal Metabolizma Hizi: {(655.0955 + 9.5634 * kilo + 1.8496 * boy - 4.6756 * yas)}" : //kadin icin  
+                                                 $"Bazal Metabolizma Hizi: {(66.473 + 13.7516 * kilo + 5.0033 * boy - 6.775 * yas)}"); //erkek icin
+        }
+    }
+}
