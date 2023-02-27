bbcf0dfa19026f17a021e0abd1bd7c8d7774dade
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 16:44:13 +0300

    idealKilo5 added

---
+++ C#/consoleApp/idealKilo5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace idealKilo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5. yontemle ideal kilo hesaplayan program
+            Console.Write("Boyunuzu giriniz(m): "); //bilgi metni
+            float boy = float.Parse(Console.ReadLine()); //girilen degeri float'a cast et ve ata 
+
+            Console.Write("Kilonuzu giriniz(kg): ");
+            float kilo = float.Parse(Console.ReadLine());
+
+            Console.Write("Cinsiyetinz kadin mi?(true/false):");
+            bool cinsiyet = bool.Parse(Console.ReadLine());
+
+            //yagsiz beden agirligi
+            Console.WriteLine(cinsiyet == true ? $"Yagsiz beden agirligi: {(1.07 * kilo - 148 * Math.Pow(kilo, 2) / (Math.Pow(100 * boy, 2)))}" :
+                                                 $"Yagsiz beden agirligi: {(1.1 * kilo - 128 * Math.Pow(kilo, 2) / (Math.Pow(100 * boy, 2)))}");
+        }
+    }
+}
