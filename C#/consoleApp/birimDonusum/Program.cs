fc9b38eedec19bffba8180e3a3c995db75ca01b4
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 14:58:54 +0300

    birimDonusum added

---
+++ C#/consoleApp/birimDonusum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security;
+
+namespace birimDonusum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //metre cinsinden girilen degeri donusturme
+            Console.Write("Donusturmek istediginiz degeri giriniz (m): ");//bilgi metni
+            float deger = float.Parse(Console.ReadLine());//girilen degeri float'a cast et ve ata
+
+            Console.Write("\nDonusum yapmak istediginiz birimi seciniz \n1-mm \n2-cm \n3-dm \n4-km \nseciminiz: ");
+            int sec = int.Parse(Console.ReadLine());
+
+            //Switch Expression 
+            float sonuc =sec switch{
+                1 => sonuc = deger * 1000, //mm donusumu
+                2 => sonuc = deger * 100, //cm donusumu
+                3 => sonuc = deger * 10, //dm donusumu
+                _ => sonuc = deger/1000 //km donusumu
+            };
+
+            Console.WriteLine($"sonuc:{sonuc}");
+
+        }
+    }
+}
