520d9bab9a0cef4d5b2a026191c96956a18281c8
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 20:35:43 +0300

    bazalMetabolizmaHizi3 added

---
+++ C#/consoleApp/bazalMetabolizmaHizi3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Transactions;
+
+namespace bazalMetabolizmaHizi3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Owen denklemi ile bazal metabolizma hizi 
+
+            Console.Write("Kilonuzu giriniz (kg): "); //bilgi metni
+            float kilo = float.Parse(Console.ReadLine()); //girilen verileri float'a cast et ve ata
+
+            Console.Write("Cinsiyetiniz kadin mi ? (true/false): "); //true=kadin | false=erkek
+            bool cinsiyet = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine(cinsiyet == true ? $"Bazal Metabolizma Hizini: {(795f + 7.18f * kilo)}":  //kadin
+                                                 $"Bazal Metabolizma Hizini: {(879f + 10.2f * kilo)}"); //erkek
+        }
+    }
+}
