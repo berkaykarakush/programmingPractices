adc303935cef03a864b41645b6d3d67a192c200f
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 20:40:46 +0300

    bazalMetabolizmaHizi5 added

---
+++ C#/consoleApp/bazalMetabolizmaHizi5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bazalMetabolizmaHizi5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dunya Saglik Orgutu Pratik Hesaplama Formulu ile bazal metabolizma hizi 
+
+            Console.Write("Kilonuzu giriniz (kg): "); //bilgi metni
+            float kilo = float.Parse(Console.ReadLine()); //girilen verileri float'a cast et ve ata
+
+            Console.Write("Cinsiyetiniz kadin mi ? (true/false): "); //true=kadin | false=erkek
+            bool cinsiyet = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine(cinsiyet == true ? $"Bazal Metabolizma Hizini: {(22.8f * kilo)}" :  //kadin
+                                                 $"Bazal Metabolizma Hizini: {(24f * kilo)}"); //erkek
+        }
+    }
+}
