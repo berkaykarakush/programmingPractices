f5e1e9d42902e5ab388f7f68c384d544ab3a3d9b
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 14:07:16 +0300

    fonksiyon added

---
+++ C#/consoleApp/fonksiyon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace fonksiyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bir y=f(x) icin x ve y hesaplama
+            //f(x) = 1, x<0 | x, 0<=x<=2 | 3, 2<x<4 | 4-x, 4<x
+
+            Console.Write("x degerini gir: "); //bilgi metni
+            float x = float.Parse(Console.ReadLine());//girilen degeri float'a cast et ve ata
+            float y=0;//ilk deger atama
+
+            Console.WriteLine(x < 0 ? $"y = {y=1}" ://x<0
+                             (0 <= x && x <= 2)? $"y = {y=x}" ://0<=x<=2
+                             (x>2 && x<4) ? $"y = {y=3}"//2<x<4
+                             : $"y = {y=4-x}");//4-x 
+        }
+    }
+}
