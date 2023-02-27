2abbca04cc7c6e657dda1adb8e3f8277a82274d4
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 14:29:56 +0300

    signumFonksiyonu2 added

---
+++ C#/consoleApp/signumFonksiyonu2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace signumFonksiyonu2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //signum(isaret) fonksiyonu
+            //sgn(f(x)) = -1, f(x)<0 | 0, f(x)=0 | 1, f(x) > 0
+
+            Console.Write("x degerini giriniz: "); //bilgi metni
+            float x = float.Parse(Console.ReadLine());//girilen degeri float'a cast et ve ata
+
+            //bilincli tur donusumu
+            float fx = (float)Math.Pow(x, 2) - 5 * x + 3; //fx = x^2 - 5x + 3
+
+            Console.WriteLine(Math.Abs(fx) / fx); //mutlak(f(x)) / f(x)
+        }
+    }
+}
