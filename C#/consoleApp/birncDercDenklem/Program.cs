c59673c2b590ff87a565fb9978886198998a4fca
berkaykarakush <berkaykaraks@outlook.com>
2023-02-27 09:54:45 +0300

    birncDercDenklem added

---
+++ C#/consoleApp/birncDercDenklem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace birncDercDenklem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //birinci derece denklem hesabi 
+
+            Console.Write("ax+b=c \na degerini giriniz: "); //bilgi metni
+            float a = float.Parse(Console.ReadLine());//girilen degeri float'a cast et ve ata
+
+            Console.Write("b degerini giriniz: ");
+            float b = float.Parse(Console.ReadLine());
+
+            Console.Write("c degerini giriniz: ");
+            float c = float.Parse(Console.ReadLine());
+
+            Console.WriteLine((c - b) / a);// x = c-b / a
+
+        }
+    }
+}
