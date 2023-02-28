8aa520cfced92618db478a797f08adc970937f9b
berkaykarakush <berkaykaraks@outlook.com>
2023-02-28 16:06:42 +0300

    ucBesKati added

---
+++ C#/consoleApp/ucBesKati/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ucBesKati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7'den buyuk sayilar 3 ve 5 in katlari olarak yazilabilirler.Girilen tamsayi icin 3 ve 5'in katlarini bulan prgram
+            Console.Write("Sayi degerini giriniz: "); //bilgi metni
+            int sayi = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            for (int a = 0; a < sayi/3; a++)//3'un kati
+            {
+                for (int b = 0; b < sayi/5; b++)//5'in kati
+                {
+                    if(3*a + 5*b == sayi)
+                    {
+                        Console.WriteLine($"3'un kati: {a} | 5'in kati: {b}");
+                    }
+                }
+            }
+        }
+    }
+}
