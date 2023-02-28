4f714b4678d8a8ed7796fd4eb32baeb835bbb129
berkaykarakush <berkaykaraks@outlook.com>
2023-02-28 19:02:45 +0300

    tersYaz added

---
+++ C#/consoleApp/tersYaz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace tersYaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //girilen tamsayiyi tersten yazdiran program
+
+            Console.Write("Tersten yazdirilmak istenen sayiyi giriniz: "); //bilgi metni
+            int sayi = int.Parse(Console.ReadLine()); //girilen degeri int'a cast et ve ata 
+
+            int kalan = 0;//degisken tanimlama ve ilk deger atama 
+            while (!(sayi == 0)) //kalan <> 0 
+            {
+                kalan = sayi % 10; // sayinin en sagindaki rakami aldik
+                Console.Write($"{kalan}"); 
+                sayi = sayi / 10; //sayi degerini 10'a bolerek sonsuz donguden kurtulduk
+            }
+        }
+    }
+}
