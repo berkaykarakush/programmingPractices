7bcd5029eacd1bd91f4bc68a85f9ed7d8b1bd247
berkaykarakush <berkaykaraks@outlook.com>
2023-02-28 16:54:34 +0300

    basamakToplami added

---
+++ C#/consoleApp/basamakToplami/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace basamakToplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //basamaklarinin toplami basamak sayisina esit olan 100-999 arasindaki tamsayilari bulan program
+            Console.WriteLine("basamaklarinin toplami basamak sayisina esit olan 100-999 arasindaki tamsayilari bulan program\n");//bilgi metni
+            for (int i = 1; i < 9; i++) //100'ler basamagi
+            {
+                for (int j = 0; j < 9; j++) //10'lar basamagi
+                {
+                    for (int k = 0; k < 9; k++) //1'ler basamagi 
+                    {
+                        if (i+j+k == 3) //basamak toplami 3 olma durumu cunku btoplami == bsayisi
+                        {
+                            Console.WriteLine($"{100*i} + {10*j} + {k} = {100*i+10*j+k}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
