9e08444c2e19c681f82b7da3a7a996491f836462
berkaykarakush <berkaykaraks@outlook.com>
2023-02-28 14:42:40 +0300

    tamsayiKontrol added

---
+++ C#/consoleApp/tamsayiKontrol/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tamsayiKontrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pozitif bir sayinin, tamsayi olup olmadigini kontrol eden program
+
+            Console.Write("Pozitif bir sayi giriniz: ");//bilgi metni
+            float sayi = float.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            //girilen deger == yuvarlanmis hali 
+            Console.WriteLine(sayi == Math.Round(sayi) ? $"{sayi} sayisi bir tamsayidir." : //==
+                                                         $"{sayi} sayisi bir tamsayi degildir."); //!=
+        }
+    }
+}
