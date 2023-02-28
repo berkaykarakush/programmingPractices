551344f6e6f4c56b577618c918f188c0ec3071cd
berkaykarakush <berkaykaraks@outlook.com>
2023-02-28 14:44:52 +0300

    tamsayiKontrol2 added

---
+++ C#/consoleApp/tamsayiKontrol2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tamsayiKontrol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pozitif bir sayinin, tamsayi olup olmadigini kontrol eden program
+
+            Console.Write("Pozitif bir sayi giriniz: ");//bilgi metni
+            float sayi = float.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            //herhangi bir sayinin 1'e gore modu 0 ise tamsayidir
+            Console.WriteLine(sayi % 1 == 0 ? $"{sayi} sayisi bir tamsayidir." : //==
+                                              $"{sayi} sayisi bir tamsayi degildir."); //!=
+        }
+    }
+}
