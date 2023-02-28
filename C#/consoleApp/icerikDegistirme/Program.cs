624db4c68e715498e90bde502610d93393a124a1
berkaykarakush <berkaykaraks@outlook.com>
2023-02-28 14:20:24 +0300

    icerikDegistirme added

---
+++ C#/consoleApp/icerikDegistirme/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace icerikDegistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //iki sayinin aktarildigi degiskenlerin iceriklerini karsilikli olarak degistiren program 
+
+            Console.Write("1.Sayiyi giriniz: ");//bilgi metni
+            int birinciSayi = int.Parse(Console.ReadLine()); //girilen degeri int'a cast et ve ata
+
+            Console.Write("2.Sayiyi giriniz: ");
+            int ikinciSayi = int.Parse(Console.ReadLine());
+
+            int tut = 0; //degisken tanimlama ve ilk deger atama 
+            tut = birinciSayi; //birinciSayi farkli bir degiskende tutuluyor
+            birinciSayi = ikinciSayi; //ikinciSayi birinciSayi'ya aktarildi
+            ikinciSayi = tut; //birinciSayi ikinciSayi'ya aktarildi
+
+            Console.WriteLine($"1.Sayi degeri: {birinciSayi} \n2.Sayi degeri: {ikinciSayi}");
+
+        }
+    }
+}
