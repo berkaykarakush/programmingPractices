6bb20018819855ef403e480597416cea88489dca
berkaykarakush <berkaykaraks@outlook.com>
2023-02-28 13:25:01 +0300

    operatorsuzBolme added

---
+++ C#/consoleApp/operatorsuzBolme/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace operatorsuzBolme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //x / y icin x = bolen.y + kalan 'dir 
+
+            Console.Write("Pay: "); //bilgi metni 
+            float pay = float.Parse(Console.ReadLine()); //girilen metni float'a cast et ve ata
+
+            Console.Write("Payda: ");
+            float payda = float.Parse(Console.ReadLine());
+
+            float bolen = 0, kalan = pay; //degisken tanimlama ve ilk deger atama 
+
+            while(kalan >= payda)
+            {
+                kalan -= payda; //kalan = kalan - payda;
+                bolen += 1; //bilen = bolen + 1;
+            }
+
+            Console.WriteLine($"Bolen: {bolen} \nKalan: {kalan}");
+            
+        }
+    }
+}
