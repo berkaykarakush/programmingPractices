4a9ea49471b32f124af74183cc38fc4ca23c2383
berkaykarakush <berkaykaraks@outlook.com>
2023-02-28 18:22:08 +0300

    beseYuvarlama added

---
+++ C#/consoleApp/beseYuvarlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace beseYuvarlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //girilen tamsayiyi, kendisinden kucuk en yakin 5'in katina yuvarlayan program 
+
+            Console.Write("Yuvarlamak istediginiz sayiyi giriniz: ");//bilgi metni
+            int sayi = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            Console.WriteLine($"En yakin 5'in katina yuvarlanmis hali: {(Math.Ceiling((double)(((sayi * 2) / 10) * 5)))}"); // ((sayi*2)/10)*5
+        }
+    }
+}
