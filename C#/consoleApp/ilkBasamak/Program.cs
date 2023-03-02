6c88f1639c29ef9998c63313fd7b1c4192c161c8
berkaykarakush <berkaykaraks@outlook.com>
2023-03-02 16:18:46 +0300

    ilkBasamak added

---
+++ C#/consoleApp/ilkBasamak/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ilkBasamak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a tamsayisi icin 1/a bolumun ilk b basamagini gosteren program
+
+            Console.Write("Bir tamsayi degeri giriniz: ");//bilgi metni
+            int sayi = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            Console.Write("Basamak degerini giriniz: "); 
+            int basamak = int.Parse(Console.ReadLine());
+
+            int i = 0, r = 1; //degisken tanimlama ve ilk deger atama 
+
+            Console.Write($"1 / {sayi} = 0."); //on metin
+
+            while (!(i == basamak)) //i <> basamak
+            {
+                Console.Write($"{(10*r/sayi)}"); 
+                r = 10 * r % sayi; //r = 10 * r mod sayi
+                i++; //arttir
+            }
+        }
+    }
+}
