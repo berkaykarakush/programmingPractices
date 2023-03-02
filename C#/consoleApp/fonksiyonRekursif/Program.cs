7b0d3b76c441c923bd905f49c5389facd7feac4d
berkaykarakush <berkaykaraks@outlook.com>
2023-03-02 11:30:09 +0300

    fonksiyonRekursif added

---
+++ C#/consoleApp/fonksiyonRekursif/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fonksiyonRekursif
+{
+    class Program
+    {
+        public static int faktoriyel(int k) 
+        {
+            if (k <= 1) return 1; //0!=1 | 1!=1
+            else return k * faktoriyel(k - 1); //3.2.1! seklinde 
+        }
+        static void Main(string[] args)
+        {
+            //faktoriyel islemini rekursif olarak hesaplama 
+
+            Console.Write("Sayi: "); //bilgi metni
+            int sayi = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            Console.WriteLine($"{sayi}! = {(faktoriyel(sayi))}"); //fonksiyonu cagir ve yaz
+        }
+
+
+    }
+}
