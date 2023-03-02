ff04a71967faae8fa16754f02b9153d130c6fc56
berkaykarakush <berkaykaraks@outlook.com>
2023-03-02 16:40:34 +0300

    birlerBasamagi added

---
+++ C#/consoleApp/birlerBasamagi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace birlerBasamagi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //girilen n adet pozitif tamsayinin sadece birler basamaklarini toplayan program
+
+            Console.Write("Kac adet sayi gireceksiniz: "); //bilgi metni
+            int n = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            int toplam = 0; //degerleri toplamak icin kullanilacak
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Sayi degerini giriniz:");
+                int sayi = int.Parse(Console.ReadLine());
+
+                toplam += sayi % 10; //birler basamagini al, toplam = toplam + sayi % 10
+            }
+            Console.Write($"Toplam = {toplam}");
+
+
+        }
+    }
+}
