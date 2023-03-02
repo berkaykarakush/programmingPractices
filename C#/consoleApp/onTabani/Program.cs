9971e4227005f58fd1ed795212efc0557d502295
berkaykarakush <berkaykaraks@outlook.com>
2023-03-02 16:07:46 +0300

    onTabani added

---
+++ C#/consoleApp/onTabani/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace onTabani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //girilen sayinin 10 tabanindaki gosterimi
+
+            Console.Write("Pozitif bir tam sayi degeri giriniz: "); //bilgi metni
+            int sayi = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            int i = sayi; //islemde kullanicak olan sayi degeri
+            int sayac = 0; //gosterimde artan deger olarak kullanildi
+
+            Console.Write("On Tabaninda gosterim = ");
+
+            //sayiyi rakamlarina parcalama islemi
+            while (!(i == 0)) //i<>0
+            {
+                Console.Write($" {(i % 10)}.10^{sayac} + "); //son basamagi yaz
+                i = (int)(i / 10); //i = i /10 , bolme islemi ile kalan basamagi al
+                sayac += 1; //bir artir
+            }
+        }
+    }
+}
