6a3d62afe81b5616bacabb879d45b973e4dda4dd
berkaykarakush <berkaykaraks@outlook.com>
2023-03-02 11:13:11 +0300

    lnAcilimi added

---
+++ C#/consoleApp/lnAcilimi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime;
+
+namespace lnAcilimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ln(2) = 1/2 + 1/12 + 1/30 + 1/56 + 1/90 + .. seklinde hesaplanabilmektedir. Paylar surekli 1 paydalar da ardisik sayilarin carpimidir. Yani 2=1*2 , 12=3*4, 30=5*6..
+
+            Console.Write("ln(n) icin \nn degerini giriniz: "); //bilgi metni
+            int n = int.Parse(Console.ReadLine()); //girilen degeri int'a cast et ve ata
+
+            float toplam = 0;//degisken tanimalama ve ilk deger atama
+            for (int k = 0; k < n; k++) // k <= n-1
+            {
+                // 1 / (k+1)*(k+2)
+                toplam += 1/((2 * k + 1) * (2 * k + 2)); //t = t + 1/((2*k+1)*(2*k+2))
+            }
+
+            Console.WriteLine($"ln{n} = {toplam}");
+
+        }
+    }
+}
