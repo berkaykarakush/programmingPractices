8847a78a44ae7e72bf2dfc6aadc179cd0831400d
berkaykarakush <berkaykaraks@outlook.com>
2023-03-02 16:53:55 +0300

    enBuyukCift added

---
+++ C#/consoleApp/enBuyukCift/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace enBuyukCift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //girilen n adet tam sayi icerisinden en buyuk cift sayiyi bulan program
+
+            Console.Write("Kac adet sayi gireceksiniz: "); //bilgi metni
+            int n = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            int enBuyukSayi = -1; //degisken tanmimlama ve ilk deger atama 
+
+            for (int i = 0; i < n; i++) //istenen adet kadar sayi girisini sagla
+            {
+                Console.Write("Sayi degerini giriniz: ");
+                int sayi = int.Parse(Console.ReadLine());
+
+                if ((sayi % 2 == 0) && (sayi > enBuyukSayi)) //sayi cift ve son  enBuyukSayi degerinden buyukse tetiklenir
+                    enBuyukSayi = sayi; //enBuyukSayiyi guncelle
+            }
+
+            Console.WriteLine(enBuyukSayi == -1 ? "Cift tam sayi yok." : //cift sayi girilmemisse 
+                                                    $"En buyuk cift tamsayi degeri: {enBuyukSayi}"); //girilen en buyuk cift sayiyi yaz
+        }
+    }
+}
