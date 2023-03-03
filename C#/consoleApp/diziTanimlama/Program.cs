784a361bb1c47b6fd18e8bd0658ec01c9786095e
berkaykarakush <berkaykaraks@outlook.com>
2023-03-03 21:05:17 +0300

    diziTanimlama added

---
+++ C#/consoleApp/diziTanimlama/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace diziTanimlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //girilen N adet verinin A dizisine atanmasi 
+
+            Console.Write("Kac adet sayi gireceginizi belirtiniz: "); //bilgi metni
+            int sayi = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+            int[] sayilar = new int[sayi];//sayi degiskeninin degeri kadar elemaani olan dizi olustur
+
+            for (int i = 0; i<sayi;i++)
+            {
+                sayilar[i] = i; //donguden gelen degeri diziye ekle
+                Console.WriteLine($"Dizinin {i+1}. Elemani: {i}"); //1. degerden baslayarak yaz, i degerini dondur
+
+            }
+        }
+    }
+}
