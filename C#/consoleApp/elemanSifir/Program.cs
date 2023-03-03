40b8f58fbcf4b868cba5ac32a5e8c3fadf6c36e9
berkaykarakush <berkaykaraks@outlook.com>
2023-03-03 21:09:29 +0300

    elemanSifir added

---
+++ C#/consoleApp/elemanSifir/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Timers;
+
+namespace elemanSifir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tum elemanlari sifir olan bir dizi olusturma
+            Console.Write("Dizinin eleman sayisini giriniz: ");//bilgi metni
+            int elemanSayisi = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve ata
+
+            int[] dizi = new int[elemanSayisi]; //girilen deger kadarlik dizi tanimla 
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                dizi[i] = 0; //tum elemanlara sifir ata
+                Console.WriteLine($"Dizinin {i+1}. elemani: {dizi[i]}");
+            }
+        }
+    }
+}
