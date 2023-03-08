64dc587650a8733798df87110e7597484911bc13
berkaykarakush <berkaykaraks@outlook.com>
2023-03-08 10:54:19 +0300

    diziTopla added

---
+++ C#/consoleApp/diziTopla/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace diziTopla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //iki diziyi ucuncu dizi uzerine toplayan program
+
+            Console.Write("Dizilerin eleman sayisini giriniz: ");//bilgi metni
+            int elemanSayisi = int.Parse(Console.ReadLine());//eleman sayisini al
+
+            //1'nci dizi
+            int[] birinciDizi = new int[elemanSayisi]; //1'nci dizi tanim
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                Console.Write($"{i + 1}'nci eleman: ");
+                birinciDizi[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("******************");
+            //2'nci dizi
+            int[] ikinciDizi = new int[elemanSayisi]; //2'nci dizi tanimlama
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                Console.Write($"{i + 1}'nci eleman: ");
+                ikinciDizi[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("******************");
+            //3'ncu dizi
+            int[] ucuncuDizi = new int[elemanSayisi];
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                ucuncuDizi[i] = birinciDizi[i] + ikinciDizi[i]; //iki dizi toplama
+                Console.WriteLine($"A[{i+1}]+B[{i+1}] = {ucuncuDizi[i]}");
+            }
+        }
+    }
+}
