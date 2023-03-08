69a7afa31e21b976ec706aea6d01f5cbe77c7e1b
berkaykarakush <berkaykaraks@outlook.com>
2023-03-08 10:32:19 +0300

    elemanTopla added

---
+++ C#/consoleApp/elemanTopla/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Timers;
+
+namespace elemanTopla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tum elemanlari toplayan program
+
+            Console.Write("Kac adet eleman gireceksiniz ?: ");//bilgi metni
+            int elemanSayisi = int.Parse(Console.ReadLine());   //girilen degeri int'a cast et ve ata
+
+            //elemanlari tek tek al
+            int[] eleman = new int[elemanSayisi]; //girilen deger kadarlik dizi tanimla
+            for (int i = 0; i < elemanSayisi; i++)
+            {
+                Console.Write($"{i + 1}. eleman: "); //bilgi metni
+                eleman[i] = int.Parse(Console.ReadLine());//girilen degeri int'a cast et ve dizinin i'nci elemanina ata
+            }
+
+            int toplam = 0;//degisken tanimlama ve ilk deger atama
+            for (int i = 0; i < elemanSayisi; i++)//girilen eleman sayisi kadar don
+            {
+                toplam += eleman[i]; //toplam = toplam + eleman[i]
+            }
+
+            Console.WriteLine($"Toplam: {toplam}");//sonuc yaz
+
+
+        }
+    }
+}
